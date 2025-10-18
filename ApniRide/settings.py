c14f3cbc59584b92195165869447ab37de23d041
Yogeshwaran <coderegime@gmail.com>
2025-10-18 17:10:48 +0530

    Added domain name in cors allowed origins.

--- ApniRide/settings.py
+++ ApniRide/settings.py
@@ -37,10 +37,12 @@ ALLOWED_HOSTS = ["*"]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:2000",
     "http://localhost:4200",
+    "https://admin.apniride.org"
 ]
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:4200",
     "http://127.0.0.1:4200",
+    "https://admin.apniride.org"
 ]
 # Application definition
 LOGGING = {
